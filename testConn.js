be4f4edb6f20eb1d9b9b79314f3756c465a70713
jonsibar <jonsibarr@gmail.com>
2024-05-27 17:33:13 +0700

    added .env, updated .gitignore and config.js, tested db connection

---
+++ testConn.js
@@ -0,0 +1,13 @@
+// testConnection.js
+const { sequelize } = require('./models');
+
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+
+testConnection();
